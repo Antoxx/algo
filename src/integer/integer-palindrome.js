2cb6f6899a82202dc9f8c101a53106cddd67e7b5
Anton Stydenko <stydenko.anton@gmail.com>
2023-03-04 16:10:36 +0300

    Roman string to integer

---
+++ src/integer/integer-palindrome.js
@@ -0,0 +1,35 @@
+import assert from 'assert';
+
+// Palindromes are 12321, 121, 11
+
+function isPalindrome(x) {
+  if (x < 0) {
+    return false;
+  }
+
+  if (x >= 0 && x < 10) {
+    return true;
+  }
+
+  let temp = x;
+  let reversed = 0;
+  while (temp) {
+    reversed = reversed * 10 + (temp % 10);
+    temp = Math.floor(temp / 10);
+  }
+
+  return x === reversed;
+}
+
+function test(fn) {
+  assert.deepStrictEqual(fn(-5), false, fn.name);
+  assert.deepStrictEqual(fn(0), true, fn.name);
+  assert.deepStrictEqual(fn(5), true, fn.name);
+  assert.deepStrictEqual(fn(11), true, fn.name);
+  assert.deepStrictEqual(fn(12), false, fn.name);
+  assert.deepStrictEqual(fn(121), true, fn.name);
+  assert.deepStrictEqual(fn(11211), true, fn.name);
+  assert.deepStrictEqual(fn(1234), false, fn.name);
+}
+
+test(isPalindrome);
---
+++ src/string/roman-to-integer.js
@@ -0,0 +1,54 @@
+import assert from 'assert';
+
+// Palindromes are "wow", "kayak", "rotator", "anna", "noon"
+
+function romanToInteger(s) {
+  let cnt = s.length;
+  if (cnt === 0) {
+    return 0;
+  }
+
+  const hash = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  const invertHash = {
+    I: ['V', 'X'],
+    X: ['L', 'C'],
+    C: ['D', 'M'],
+  };
+
+  let int = 0;
+
+  let i = cnt;
+  while (i--) {
+    const cur = s[i];
+    let tmp = hash[cur];
+
+    if (s[i + 1] && invertHash[cur] && invertHash[cur].includes(s[i + 1])) {
+      tmp *= -1;
+    }
+
+    int += tmp;
+  }
+
+  return int;
+}
+
+function test(fn) {
+  assert.deepStrictEqual(fn(''), 0, fn.name);
+  assert.deepStrictEqual(fn('I'), 1, fn.name);
+  assert.deepStrictEqual(fn('III'), 3, fn.name);
+  assert.deepStrictEqual(fn('V'), 5, fn.name);
+  assert.deepStrictEqual(fn('IV'), 4, fn.name);
+  assert.deepStrictEqual(fn('LVIII'), 58, fn.name);
+  assert.deepStrictEqual(fn('MCMXCIV'), 1994, fn.name);
+}
+
+test(romanToInteger);
