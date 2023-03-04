9a63ffd6c9edd2582cb39a8a1a48240eebadde4b
Anton Stydenko <stydenko.anton@gmail.com>
2023-03-04 16:10:03 +0300

    Array two sum problem

---
+++ src/array-two-sum.js
@@ -0,0 +1,49 @@
+/**
+ * Find indexes of two different integers in an array which sum is equal to N
+ * [2,6,3,21] for N=5 gives [0,2]
+ */
+
+import assert from 'assert';
+
+function twoSumBruteForce(nums, target) {
+  let ticks = 0;
+  const ln = nums.length;
+  for (let i = 0; i < ln; i++) {
+    ticks++;
+    for (let j = i + 1; j < ln; j++) {
+      ticks++;
+      if (nums[i] + nums[j] === target) {
+        console.log(`Ticks for brute force: ${ticks}`);
+        return [i, j];
+      }
+    }
+  }
+  return null;
+}
+
+function twoSumDiff(nums, target) {
+  let ticks = 0;
+  let hash = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    ticks++;
+    let diff = target - nums[i];
+    if (hash.has(diff)) {
+      console.log(`Ticks for diff + hash: ${ticks}`);
+      return [hash.get(diff), i];
+    }
+
+    hash.set(nums[i], i);
+  }
+}
+
+function test(fn) {
+  assert.deepStrictEqual(fn([3, 3], 6), [0, 1], fn.name);
+  assert.deepStrictEqual(fn([2, 6, 3, 21], 5), [0, 2], fn.name);
+  assert.deepStrictEqual(fn([2, 7, 11, 15], 17), [0, 3], fn.name);
+  assert.deepStrictEqual(fn([2, 7, 11, 15], 18), [1, 2], fn.name);
+  assert.deepStrictEqual(fn([2, 7, 11, 15], 26), [2, 3], fn.name);
+  assert.deepStrictEqual(fn([2, 7, 11, 7], 14), [1, 3], fn.name);
+}
+
+test(twoSumBruteForce);
+test(twoSumDiff);
