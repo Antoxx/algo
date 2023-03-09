c92e25546633df1d5cd31569eeae8dc28bf21651
Anton Stydenko <stydenko.anton@gmail.com>
2023-03-09 11:16:05 +0300

    Reord + new

---
+++ src/string/find-index-of-first-occurrence.js
@@ -0,0 +1,47 @@
+import assert from 'assert';
+
+/**
+ * 28. Find the Index of the First Occurrence in a String
+ *
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+ */
+
+function strStr(haystack, needle) {
+  let firstIdx = -1;
+  let nextIdx = 0;
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[nextIdx]) {
+      if (firstIdx === -1) {
+        firstIdx = i;
+      }
+
+      let currNeedleLn = i - firstIdx + 1;
+      if (currNeedleLn === needle.length) {
+        return firstIdx;
+      }
+
+      nextIdx++;
+    } else {
+      if (firstIdx !== -1) {
+        i = firstIdx;
+        firstIdx = -1;
+      }
+
+      nextIdx = 0;
+    }
+  }
+
+  return -1;
+}
+
+function test(fn) {
+  assert.deepStrictEqual(fn('sadbutsad', 'sad'), 0, fn.name);
+  assert.deepStrictEqual(fn('leetcode', 'leeto'), -1, fn.name);
+  assert.deepStrictEqual(fn('hello', 'll'), 2, fn.name);
+  assert.deepStrictEqual(fn('mississippi', 'issip'), 4, fn.name);
+  assert.deepStrictEqual(fn('mississippi', 'pi'), 9, fn.name);
+}
+
+test(strStr);
---
+++ src/string/generate-parentheses.js
@@ -0,0 +1,50 @@
+/**
+ * 22. Generate Parentheses
+ *
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * https://leetcode.com/problems/generate-parentheses/
+ *
+ * Generate all well-formed bracket sequences using parenthesis "(" and ")" with the length equal to 2\*n:
+ * n = 1
+ * ()
+ *
+ * n = 2
+ * ()()
+ * (())
+ *
+ * n = 3
+ * ()()()
+ * (())()
+ * ()(())
+ * (()())
+ * ((()))
+ */
+
+function generateParenthesis(n) {
+  const ans = [];
+
+  function generate(n, open = 0, closed = 0, current = '') {
+    if (n === 0 || (current.length > 0 && current.length >= 2 * n)) {
+      console.log(current);
+      return ans.push(current);
+    }
+
+    if (open < n) {
+      generate(n, open + 1, closed, current + '(');
+    }
+
+    if (closed < open) {
+      generate(n, open, closed + 1, current + ')');
+    }
+  }
+
+  generate(n);
+
+  return ans;
+}
+
+generateParenthesis(0);
+generateParenthesis(1);
+generateParenthesis(2);
+generateParenthesis(3);
