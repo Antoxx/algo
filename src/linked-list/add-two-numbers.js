11f1d44ef135f57083808cabadf9152a638bbfa6
Anton Stydenko <stydenko.anton@gmail.com>
2023-03-12 16:13:08 +0300

    Linked list changes

--- src/linked-list/add-two-numbers.js
+++ src/linked-list/add-two-numbers.js
@@ -1,4 +1,11 @@
 /**
+ * 2. Add Two Numbers
+ *
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
  * https://leetcode.com/problems/add-two-numbers/
  */
 
@@ -10,22 +17,22 @@ function addTwoNumbers(l1, l2) {
   let list = resList;
   let curList1 = l1;
   let curList2 = l2;
-  let radixMoving = false;
+  let carryMoving = false;
 
   while (true) {
     const val1 = curList1?.val || 0;
     const val2 = curList2?.val || 0;
-    const radixVal = +radixMoving;
+    const carryVal = +carryMoving;
 
-    const sum = val1 + val2 + radixVal;
+    const sum = val1 + val2 + carryVal;
 
-    radixMoving = sum >= 10;
-    list.val = radixMoving ? sum - 10 : sum;
+    carryMoving = sum >= 10;
+    list.val = carryMoving ? sum - 10 : sum;
 
     curList1 = curList1?.next;
     curList2 = curList2?.next;
 
-    if (!curList1 && !curList2 && !radixMoving) {
+    if (!curList1 && !curList2 && !carryMoving) {
       break;
     } else {
       list.next = new ListNode();
