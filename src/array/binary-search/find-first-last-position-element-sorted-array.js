957ca1cdd1beb019e9f38ac9b8d2f8adbf9b3417
Anton Stydenko <stydenko.anton@gmail.com>
2025-06-22 21:44:04 +0300

    Changes

--- src/array/binary-search/find-first-last-position-element-sorted-array.js
+++ src/array/binary-search/find-first-last-position-element-sorted-array.js
@@ -3,6 +3,8 @@ import assert from 'assert';
 /**
  * 34. Find First and Last Position of Element in Sorted Array
  *
+ * Medium
+ *
  * Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
  * If target is not found in the array, return [-1, -1].
  *
--- src/array/binary-search/search-index-position.js
+++ src/array/binary-search/search-index-position.js
@@ -3,6 +3,8 @@ import assert from 'assert';
 /**
  * 35. Search Insert Position
  *
+ * Easy
+ *
  * Given a sorted array of distinct integers and a target value, return the index if the target is found.
  * If not, return the index where it would be if it were inserted in order.
  * You must write an algorithm with O(log n) runtime complexity.
@@ -11,32 +13,22 @@ import assert from 'assert';
  */
 
 function searchIndexPosition(nums, target) {
-  let ln = nums.length;
-  let left = 0;
-  let right = ln - 1;
+  let left = 0
+  let right = nums.length - 1
 
   while (left <= right) {
-    if (nums[left] === target) {
-      return left;
-    }
+    const mid = Math.floor((left + right) / 2)
 
-    if (nums[right] === target) {
-      return right;
-    }
-
-    let mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) {
-      return mid;
-    }
-
-    if (nums[mid] > target) {
-      right = mid - 1;
-    } else {
-      left = mid + 1;
+      return mid
+    } else if (nums[mid] > target) {
+      right = mid - 1
+    } else if (nums[mid] < target) {
+      left = mid + 1
     }
   }
 
-  return left;
+  return left
 }
 
 function searchIndexPositionSimple(nums, target) {
